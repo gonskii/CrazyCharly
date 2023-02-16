4f35d297441ab4926832bd771d6e98f77ff7bdb5
tricki-lucky7 <remi.perrier1@etu.univ-lorraine.fr>
2023-02-16 16:23:49 +0100

    maj Proposition

--- src/classes/tables/Proposition.php
+++ src/classes/tables/Proposition.php
@@ -5,14 +5,16 @@ namespace teamiut\tables;
 use teamiut\db\ConnectionFactory as ConnectionFactory;
 
 class Proposition {
-    private int $IDProposition;
     private int $IDUser;
     private string $description;
+    private string $nom;
+    private int $nbPlaceMax;
 
-    public function __construct(int $IDProposition, int $IDUser, string $description) {
-        $this->IDProposition = $IDProposition;
+    public function __construct(int $IDUser, string $description, string $nom, int $nbPlaceMax   ) {
         $this->IDUser = $IDUser;
         $this->description = $description;
+        $this->nom = $nom;
+        $this->nbPlaceMax = $nbPlaceMax;
     }
 
     public function __get($name) {
@@ -21,11 +23,12 @@ class Proposition {
 
     public function save() {
         $db = ConnectionFactory::makeConnection();
-        $sql = "INSERT INTO Proposition (IDProposition, IDUser, description) VALUES (:IDProposition, :IDUser, :description)";
+        $sql = "INSERT INTO Proposition (IDUser, description, nom, nbPlacesMax) VALUES (:IDUser, :description, :nom, :nbPlaceMax)";
         $stmt = $db->prepare($sql);
-        $stmt->bindParam(':IDProposition', $this->IDProposition);
         $stmt->bindParam(':IDUser', $this->IDUser);
         $stmt->bindParam(':description', $this->description);
+        $stmt->bindParam(':nom', $this->nom);
+        $stmt->bindParam(':nbPlaceMax', $this->nbPlaceMax);
         $stmt->execute();
     }
 
